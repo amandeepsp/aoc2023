7a701982e0420f0600bd9e9804f7071125d0dee0
Amandeep Singh <amandeepspdhr@gmail.com>
2023-12-23 15:07:41 +0530

    Day 5 part 2

--- day05.py
+++ day05.py
@@ -7,6 +7,30 @@ def transform_range(elem, ranges):
             return dest_range.start + (elem - source_range.start)
     return elem
 
+def transform_range_for_range(seed_range, ranges):
+    for curr_range in ranges:
+        source_range = curr_range["src"]
+        dest_range = curr_range["dest"]
+
+        intersected_src_seed = range(max(seed_range.start, source_range.start), min(seed_range.stop, source_range.stop))
+        if len(intersected_src_seed) > 0:
+            new_ranges = []
+            transformed_range =  range(
+                dest_range.start + (intersected_src_seed.start - source_range.start),
+                dest_range.start + (intersected_src_seed.start - source_range.start) + len(intersected_src_seed)
+            )
+            new_ranges.append(transformed_range)
+
+            if seed_range.start < intersected_src_seed.start:
+                new_ranges.append(range(seed_range.start, intersected_src_seed.start))
+            
+            if seed_range.stop > intersected_src_seed.stop:
+                new_ranges.append(range(intersected_src_seed.stop, seed_range.stop))
+
+            return new_ranges
+
+    return [seed_range]
+
 with open("data/day05.txt") as infile:
     data = infile.read().split("\n\n")
     seeds = [int(seed) for seed in data[0].split(": ")[1].split(" ")]
@@ -32,4 +56,17 @@ with open("data/day05.txt") as infile:
     print(f"Part 1: {min_final}")
 
 
+    seeds =  [range(seed, seed + range_len) for seed, range_len in zip(seeds[0::2], seeds[1::2])]
+
+    for ranges_arr in range_maps:
+        new_seeds = []
+        while len(seeds) > 0:
+            curr_seed_range = seeds.pop()
+            new_seed = transform_range_for_range(curr_seed_range, ranges_arr)
+            new_seeds.append(new_seed[0])
+            if len(new_seed) > 1:
+                seeds.extend(new_seed[1:])
+        seeds = new_seeds
     
+    min_seed = min([r.start for r in seeds])
+    print(f"Part 2: {min_seed}")
